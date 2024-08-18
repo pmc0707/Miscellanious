3e0ba82502429224cf9902a08eb1c85ea3cc98e3
pmc0707 <pranshugoodboy@gmail.com>
2024-08-18 10:50:29 +0530

    in the case of sever getting error we have to use this code

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/register', (req, res) => {
     res.send('register GET respond');
